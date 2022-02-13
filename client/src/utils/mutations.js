7953b2efedc26818e4e8e707173b7179d9fb3d2f
Alex Snyder <alystrife@gmail.com>
2022-02-12 21:58:45 -0500

    finished login and adduser mutation , and gql schema

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($authors: [String!], $description: String!, $image: String, $link: String, $bookId: ID!, $title: String!) {
+  saveBook(authors: $authors, description: $description, image: $image, link: $link, bookId: $bookId!, title: $title) {
+    _id
+    username
+    savedBooks {
+      bookId
+    }
+  }
+}
+;`
+
+export const REMOVE_BOOK = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+  ;`
+
+export const ADD_USER = gql`
+mutation login($email: String!, $password: String!) {
+  login(email: $email, password: $password) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+;`
