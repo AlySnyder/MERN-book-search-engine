a28fc888d0b6642557c515e2e7a18244f976e5ed
Alex Snyder <alystrife@gmail.com>
2022-02-13 20:45:14 -0500

    revert

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ const startServer = async () => {
   });
 
   // Start the Apollo server
-  server.start();
+  await server.start();
 
   // integrate our Apollo server with the Express application as middleware
   server.applyMiddleware({ app });
