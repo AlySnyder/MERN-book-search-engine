7ef64d104d657b45acfe5321c669cbb1f53df5b9
Alex Snyder <alystrife@gmail.com>
2022-02-13 20:49:45 -0500

    try route

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 
-  app.get("*", (req, res) => {
+  app.get(/^\/(?!graphql\/?$)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
